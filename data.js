f6b2c5924cac4a0b97f79365739ee6e0bee005e1
Joe Hart <joehart42@outlook.com>
2020-06-21 20:43:35 +0100

    add routes

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const pride = {
   name: "Pride",
   id: "pride",
-  year: "1979"
+  year: "1979",
   colors: [
     {
       name: "red",
@@ -49,35 +49,32 @@ const pride = {
 };
 
 const bisexual = {
-    name: "Bisexual",
-    id: "bisexual",
-    year: "1998"
-    colors: [
-      {
-        name: "magenta",
-        hexCode: "#d6006f",
-        r: 214,
-        g: 0,
-        b: 111,
-      },
-      {
-        name: "lavender",
-        hexCode: "#724e94",
-        r: 114,
-        g: 78,
-        b: 148,
-      },
-      {
-        name: "blue",
-        hexCode: "#0038a7",
-        r: 0,
-        g: 56,
-        b: 167,
-      },
-    ],
-  };
+  name: "Bisexual",
+  id: "bisexual",
+  year: "1998",
+  colors: [
+    {
+      name: "magenta",
+      hexCode: "#d6006f",
+      r: 214,
+      g: 0,
+      b: 111,
+    },
+    {
+      name: "lavender",
+      hexCode: "#724e94",
+      r: 114,
+      g: 78,
+      b: 148,
+    },
+    {
+      name: "blue",
+      hexCode: "#0038a7",
+      r: 0,
+      g: 56,
+      b: 167,
+    },
+  ],
+};
 
-module.exports.flags = [
-    pride,
-    bisexual
-]
+module.exports.flags = [pride, bisexual];
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { palettes } = require("./data");
+const { flags } = require("./data");
 
 const app = express();
 const port = 3000;
@@ -9,57 +9,31 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/palettes", (req, res) => {
+app.get("/flags", (req, res) => {
   const { search } = req.query;
 
   if (search) {
     res.send(
-      palettes.filter((p) =>
-        p.paletteName.toLowerCase().includes(search.toLowerCase())
+      flags.filter((f) =>
+        f.paletteName.toLowerCase().includes(search.toLowerCase())
       )
     );
   }
-  res.send(palettes);
+  res.send(flags);
 });
 
-app.get("/palettes/:id", (req, res) => {
+app.get("/flags/:id", (req, res) => {
   const { id } = req.params;
 
-  const palette = palettes.find((r) => String(r.id) === String(id));
+  const flag = flags.find((r) => String(r.id) === String(id));
 
-  if (palette) {
-    res.send(palette);
+  if (flag) {
+    res.send(flag);
   }
 
   res.status(404).send("Not found");
 });
 
-app.post("/palettes", (req, res) => {
-  const { body } = req;
-
-  if (!body.paletteName) {
-    return res.status(422).send("Parameter paletteName is required");
-  }
-
-  if (!body.colors || !Array.isArray(body.colors) || body.colors.length < 3) {
-    return res.status(422).send("Add at least 3 colors");
-  }
-
-  if (body.colors.find((color) => !color.colorName || !color.hexCode)) {
-    return res.status(422).send("Each color must have a colorName and hexCode");
-  }
-
-  const palette = {
-    id: palettes.length,
-    name: body.paletteName,
-    colors: body.colors,
-  };
-
-  palettes.push(palette);
-
-  res.status(201).send(palette);
-});
-
 app.listen(port, () =>
-  console.log(`Beautiful color palettes waiting for you at port ${port}!`)
+  console.log(`🏳️‍🌈 Pride flags being served at port ${port}!`)
 );
