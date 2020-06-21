05513353910531d6a56a759ec56b11aed87ccb51
Joe Hart <joehart42@outlook.com>
2020-06-21 20:25:14 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,83 @@
+const pride = {
+  name: "Pride",
+  id: "pride",
+  year: "1979"
+  colors: [
+    {
+      name: "red",
+      hexCode: "#e20a17",
+      r: 226,
+      g: 10,
+      b: 23,
+    },
+    {
+      name: "orange",
+      hexCode: "#fd8d25",
+      r: 253,
+      g: 141,
+      b: 37,
+    },
+    {
+      name: "yellow",
+      hexCode: "#feec34",
+      r: 254,
+      g: 236,
+      b: 52,
+    },
+    {
+      name: "green",
+      hexCode: "#108028",
+      r: 16,
+      g: 128,
+      b: 40,
+    },
+    {
+      name: "blue",
+      hexCode: "#0f54fb",
+      r: 15,
+      g: 84,
+      b: 251,
+    },
+    {
+      name: "violet",
+      hexCode: "#751086",
+      r: 117,
+      g: 16,
+      b: 134,
+    },
+  ],
+};
+
+const bisexual = {
+    name: "Bisexual",
+    id: "bisexual",
+    year: "1998"
+    colors: [
+      {
+        name: "magenta",
+        hexCode: "#d6006f",
+        r: 214,
+        g: 0,
+        b: 111,
+      },
+      {
+        name: "lavender",
+        hexCode: "#724e94",
+        r: 114,
+        g: 78,
+        b: 148,
+      },
+      {
+        name: "blue",
+        hexCode: "#0038a7",
+        r: 0,
+        g: 56,
+        b: 167,
+      },
+    ],
+  };
+
+module.exports.flags = [
+    pride,
+    bisexual
+]
