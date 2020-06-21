7f88cf9bfbffe8f1238f56afbaffb6b05a537947
Joe Hart <joehart42@outlook.com>
2020-06-21 21:16:18 +0100

    Add more flags

---
+++ flags/asexual.js
@@ -0,0 +1,43 @@
+module.exports = {
+  name: "Asexual",
+  id: "asexual",
+  year: "2010",
+  svg: `<?xml version="1.0" standalone="no"?>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 480">
+      <path fill="#fff" d="M0 0h800v480H0z"/>
+      <path d="M0 0h800v120H0z"/>
+      <path fill="purple" d="M0 360h800v120H0z"/>
+      <path fill="#a3a3a3" d="M0 120h800v120H0z"/>
+    </svg>
+      `,
+  colors: [
+    {
+      name: "black",
+      hexCode: "#000000",
+      r: 0,
+      g: 0,
+      b: 0,
+    },
+    {
+      name: "grey",
+      hexCode: "#a3a3a3",
+      r: 163,
+      g: 163,
+      b: 163,
+    },
+    {
+      name: "white",
+      hexCode: "#ffffff",
+      r: 255,
+      g: 255,
+      b: 255,
+    },
+    {
+      name: "purple",
+      hexCode: "#800080",
+      r: 128,
+      g: 0,
+      b: 128,
+    },
+  ],
+};
---
+++ flags/intersex.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "Intersex",
+  id: "intersex",
+  year: "2013",
+  svg: `<?xml version="1.0" encoding="UTF-8"?>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600">
+      <path fill="#FFD800" d="M0 0h900v600H0z"/>
+      <circle cx="450" cy="300" r="147" fill="none" stroke-width="50" stroke="#7902aa"/>
+    </svg>
+    `,
+  colors: [
+    {
+      name: "purple",
+      hexCode: "#7902aa",
+      r: 121,
+      g: 2,
+      b: 170,
+    },
+    {
+      name: "yellow",
+      hexCode: "#FFD800",
+      r: 255,
+      g: 216,
+      b: 0,
+    },
+  ],
+};
