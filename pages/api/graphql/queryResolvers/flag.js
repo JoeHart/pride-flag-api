6a9de2df6c5331ded5b571654ff0536ba36d1fa8
Joe Hart <joehart42@outlook.com>
2020-06-28 21:37:02 +0100

    remove console log

--- pages/api/graphql/queryResolvers/flag.js
+++ pages/api/graphql/queryResolvers/flag.js
@@ -2,6 +2,5 @@ import flags from "../../../../flags";
 
 export default function flag(parent, args, context) {
   const { id } = args;
-  console.log(flags);
   return flags.find((flag) => flag.id === id);
 }
