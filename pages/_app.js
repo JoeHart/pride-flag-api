15dc9627d7c87f75a6599985283234ecaaa653ce
Joe Hart <joehart42@outlook.com>
2021-04-26 21:16:30 +0100

    re add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import GlobalStyle from "../components/GlobalStyle";
 import SiteHeader from "../components/SiteHeader";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="page">
-      <SiteHeader />
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </div>
+    <PlausibleProvider domain="pride.dev">
+      <div className="page">
+        <SiteHeader />
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </div>
+    </PlausibleProvider>
   );
 }
 
