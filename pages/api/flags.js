32d96b976fa05792de26cffff88a14ba02cf6787
Joe Hart <joehart42@outlook.com>
2021-04-26 21:35:16 +0100

    add track event to flags endpoint

--- pages/api/flags.js
+++ pages/api/flags.js
@@ -1,9 +1,17 @@
 import flags from "../../data/flags";
+import Plausible from "plausible-tracker";
 
+const plausible = Plausible({
+  domain: "pride.dev",
+});
+
+const { trackPageview } = Plausible();
 export default (req, res) => {
   const {
     query: { search },
   } = req;
+
+  trackPageview({ url: "/api/flags" });
 
   if (search) {
     res.send(
