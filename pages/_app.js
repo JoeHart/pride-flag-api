f2fd58c933dbff549d31b31b5ae9b177433ea4c9
Joe Hart <joehart42@outlook.com>
2021-04-26 20:57:01 +0100

    Remove plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import GlobalStyle from "../components/GlobalStyle";
 import SiteHeader from "../components/SiteHeader";
-import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="pride.dev">
-      <div className="page">
-        <SiteHeader />
-        <Component {...pageProps} />
-        <GlobalStyle />
-      </div>
-    </PlausibleProvider>
+    <div className="page">
+      <SiteHeader />
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </div>
   );
 }
 
