4865e45fc291ce6245b2fa89942d6817a2606109
Alistair Shepherd <alistair@accudio.com>
2023-07-02 21:08:56 +0100

    feat: add bear flag

---
+++ data/flags/bear_1995.js
@@ -0,0 +1,61 @@
+import svg from "./svg/bear_1995.svg";
+
+const bear_1995 = {
+  name: "Bear",
+  id: "bear_1995",
+  year: "1995",
+  colors: [
+    {
+      name: "brown",
+      hexCode: "#623804",
+      r: 98,
+      g: 56,
+      b: 4,
+    },
+    {
+      name: "orange",
+      hexCode: "#d56300",
+      r: 213,
+      g: 99,
+      b: 0,
+    },
+    {
+      name: "yellow",
+      hexCode: "#fedd63",
+      r: 254,
+      g: 221,
+      b: 99,
+    },
+    {
+      name: "cream",
+      hexCode: "#fee6b8",
+      r: 254,
+      g: 230,
+      b: 184,
+    },
+    {
+      name: "white",
+      hexCode: "#ffffff",
+      r: 255,
+      g: 255,
+      b: 255,
+    },
+    {
+      name: "grey",
+      hexCode: "#555555",
+      r: 85,
+      g: 85,
+      b: 85,
+    },
+    {
+      name: "black",
+      hexCode: "#000000",
+      r: 0,
+      g: 0,
+      b: 0,
+    },
+  ],
+  svg,
+};
+
+export default bear_1995;
