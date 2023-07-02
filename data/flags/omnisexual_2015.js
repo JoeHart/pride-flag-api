0c249d1caa47bf9f87da0af1db92c43a4eb9d17a
Alistair Shepherd <alistair@accudio.com>
2023-07-02 20:57:36 +0100

    feat: added omnisexual flag

---
+++ data/flags/omnisexual_2015.js
@@ -0,0 +1,47 @@
+import svg from "./svg/omnisexual_2015.svg";
+
+const omnisexual_2015 = {
+  name: "Omnisexual",
+  id: "omnisexual_2015",
+  year: "2015",
+  colors: [
+    {
+      name: "lightpink",
+      hexCode: "#ff9bcf",
+      r: 255,
+      g: 155,
+      b: 207,
+    },
+    {
+      name: "pink",
+      hexCode: "#ff55c0",
+      r: 255,
+      g: 85,
+      b: 192,
+    },
+    {
+      name: "darkpurple",
+      hexCode: "#250148",
+      r: 37,
+      g: 1,
+      b: 72,
+    },
+    {
+      name: "blue",
+      hexCode: "#675eff",
+      r: 103,
+      g: 94,
+      b: 255,
+    },
+    {
+      name: "lightblue",
+      hexCode: "#8fa7ff",
+      r: 143,
+      g: 167,
+      b: 255,
+    },
+  ],
+  svg,
+};
+
+export default omnisexual_2015;
