cf43b636089a4175bd0d7d240c03167f25b0270f
Alistair Shepherd <alistair@serieseight.com>
2023-07-05 22:59:16 +0100

    feat: Added Gay Men flag

---
+++ data/flags/gay_men_2019.js
@@ -0,0 +1,61 @@
+import svg from "./svg/gay_men_2019.svg";
+
+const gay_men_2019 = {
+  name: "Gay Men",
+  id: "gay_men_2019",
+  year: "2019",
+  svg,
+  colors: [
+    {
+      name: "dark green",
+      hexCode: "#078d70",
+      r: 7,
+      g: 141,
+      b: 112,
+    },
+    {
+      name: "green",
+      hexCode: "#26ceaa",
+      r: 38,
+      g: 206,
+      b: 170,
+    },
+    {
+      name: "light green",
+      hexCode: "#99e8c2",
+      r: 153,
+      g: 232,
+      b: 194,
+    },
+    {
+      name: "white",
+      hexCode: "#ffffff",
+      r: 255,
+      g: 255,
+      b: 255,
+    },
+    {
+      name: "light blue",
+      hexCode: "#7bade3",
+      r: 123,
+      g: 173,
+      b: 227,
+    },
+    {
+      name: "purple",
+      hexCode: "#5049cb",
+      r: 80,
+      g: 73,
+      b: 203,
+    },
+    {
+      name: "dark purple",
+      hexCode: "#3e1a78",
+      r: 62,
+      g: 26,
+      b: 120,
+    },
+  ],
+};
+
+export default gay_men_2019;
